469836305afcb22461036f16d98663a8f60d3bf0
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-08-31 02:39:44 -0700

    Completed Project

--- Module1/src/edu/ser222/m01_02/CompletedMatrix.java
+++ Module1/src/edu/ser222/m01_02/CompletedMatrix.java
@@ -1,3 +1,5 @@
+package edu.ser222.m01_02;
+
 public class CompletedMatrix implements Matrix {
 
     private int[][] elements;
@@ -62,17 +64,67 @@ public class CompletedMatrix implements Matrix {
 
     @Override
     public Matrix minus(Matrix other) {
-        return null;
+        // Check if it exists
+        // Check if the dimensions match
+
+        if (other == null || other.getRows() != getRows() || other.getColumns() != getColumns()) {
+            throw new IllegalArgumentException("Matrices Dimensions Must Match");
+        }
+
+        int[][] subtractedMatrix = new int[getRows()][getColumns()];
+
+        for (int row = 0; row < getRows(); row++) {
+            for (int column = 0; column < getColumns(); column++) {
+                subtractedMatrix[row][column] = elements[row][column] - other.getElement(row,column);
+            }
+        }
+
+        return new CompletedMatrix(subtractedMatrix);
     }
 
     @Override
     public Matrix multiply(Matrix other) {
-        return null;
+        // Check if it exists
+        // Check if the dimensions match
+
+        if (other == null || getColumns() != other.getRows()) {
+            throw new IllegalArgumentException("Matrix A's column must match Matrix B's Row");
+        }
+
+        int[][] multipliedMatrix = new int[getRows()][other.getColumns()];
+
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < other.getColumns(); j++) {
+                int dotProduct = 0;
+                for (int k = 0; k < getColumns(); k++) {
+                    dotProduct += elements[i][k] * other.getElement(k, j);
+                }
+                multipliedMatrix[i][j] = dotProduct;
+            }
+        }
+
+        return new CompletedMatrix(multipliedMatrix);
     }
 
-    public boolean equals(Object other) { return false;}
-
-    public String toString(){
-        return null;
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        else return false;
     }
+
+    @Override
+    public String toString() {
+        String matrixToString = "";
+        for (int row = 0; row < getRows(); row++) {
+            for (int column = 0; column < getColumns(); column++) {
+                matrixToString += elements[row][column] + " ";
+            }
+            matrixToString += "\n";
+        }
+        return matrixToString;
+    }
+
+
 }
