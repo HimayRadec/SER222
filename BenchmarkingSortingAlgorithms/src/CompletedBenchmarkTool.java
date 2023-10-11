235c63c56f95ee08d919f8a536f6c40a7796b8d2
HimayRadec <himayradec@gmail.com>
2023-10-11 09:28:31 -0700

    Worked On Benchmarking Project

--- BenchmarkingSortingAlgorithms/src/CompletedBenchmarkTool.java
+++ BenchmarkingSortingAlgorithms/src/CompletedBenchmarkTool.java
@@ -7,6 +7,12 @@
  * @version (a version number or a date)
  */
 
+package edu.ser222.m02_01;
+
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
 
 public class CompletedBenchmarkTool implements BenchmarkTool {
     
@@ -52,16 +58,173 @@ public class CompletedBenchmarkTool implements BenchmarkTool {
     private static void exch(Comparable[] a, int i, int j) {
         Comparable t = a[i]; a[i] = a[j]; a[j] = t;
     }
-    
+
+    // Half the data is 0s, half 1s
+    @Override
+    public Integer[] generateTestDataBinary(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size must be greater than 0");
+        }
+
+        Integer[] testData = new Integer[size];
+
+        // Fill half the array with 0s
+        for (int i = 0; i < size/2; i++) testData[i] = 0;
+
+        // Fill the other half with 1s
+        for (int i = size/2; i < size; i++) testData[i] = 1;
+
+        return testData;
+    }
+
+
+    @Override
+    public Integer[] generateTestDataHalves(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size must be greater than 0");
+        }
+
+        Integer[] testData = new Integer[size];
+
+        int halfSize = size / 2;
+
+        // Fill the first half with 0s
+        for (int i = 0; i < halfSize; i++) {
+            testData[i] = 0;
+        }
+
+        // Fill the second half with consecutive integers following the pattern
+        int currentNumber = 1;
+        int currentIndex = halfSize;
+
+        // TODO: For sure some errors here
+        while (currentIndex < size) {
+            for (int i = 0; i < currentNumber && currentIndex < size; i++) {
+                testData[currentIndex] = currentNumber;
+                currentIndex++;
+            }
+            currentNumber++;
+        }
+
+        System.out.println("Generate Data Halves");
+        for (Integer value : testData) {
+            System.out.println(value);
+        }
+
+        return testData;
+    }
+
+
+    // Generates an array of integers where half the data is 0s, and half random
+    @Override
+    public Integer[] generateTestDataHalfRandom(int size) {
+        // Check if size == 0 or less
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size must be greater than 0");
+        }
+
+        Integer[] testData = new Integer[size];
+        Random random = new Random();
+
+        // Fill the first half with 0s
+        for (int i = 0; i < size/2; i++) {
+            testData[i] = 0;
+        }
+
+        for (int i = size/2; i < size; i++) {
+            testData[i] = random.nextInt(Integer.MAX_VALUE);
+        }
+
+        return testData;
+    }
+
+    @Override
+    public double computeDoublingFormula(double t1, double t2){
+        // Check for invalid times
+        if (t1 <= 0 || t2 <= 0) {
+            throw new IllegalArgumentException("Both times must be positive");
+        }
+
+        return Math.log(2) * (t2/t1);
+    }
+
+    @Override
+    public double benchmarkInsertionSort(Integer[] small, Integer[] large){
+        // TODO: Check this
+//        if (small == null || large == null || small.length == 0 || large.length != 2 * small.length) {
+//            throw new IllegalArgumentException("Invalid input arrays");
+//        }
+
+
+        // Measure the runtime of insertion sort for the small array
+        Stopwatch smallStopwatch = new Stopwatch();
+        insertionSort(small);
+        double smallEndtime = smallStopwatch.elapsedTime();
+
+        // Measure the runtime of insertion sort for the large array
+        Stopwatch largeStopwatch = new Stopwatch();
+        insertionSort(large);
+        double largeEndtime = largeStopwatch.elapsedTime();
+
+        // Compute the empirical 'b' value using the doubling formula
+
+        return computeDoublingFormula(smallEndtime, largeEndtime);
+    }
+
+    @Override
+    public double benchmarkShellsort(Integer[] small, Integer[] large){
+
+        // Check for incorrect parameter values
+//        if (small == null || large == null || small.length == 0 || large.length != 2 * small.length) {
+//            throw new IllegalArgumentException("Invalid input arrays");
+//        }
+
+        // Measure the runtime of shellsort for the small array
+        Stopwatch smallStopwatch = new Stopwatch();
+        shellsort(small);
+        double smallEndtime = smallStopwatch.elapsedTime();
+
+        // Measure the runtime of shellsort for the large array
+        Stopwatch largeStopwatch = new Stopwatch();
+        shellsort(large);
+        double largeEndtime = largeStopwatch.elapsedTime();
+
+        return computeDoublingFormula(smallEndtime, largeEndtime);
+    }
+
+    @Override
+    public void runBenchmarks(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size must be greater than 0");
+        }
+
+        // Benchmark insertion sort and shellsort for each dataset
+        double bValueInsertionBinary = benchmarkInsertionSort(generateTestDataBinary(size), generateTestDataBinary(size*2));
+        double bValueInsertionHalves = benchmarkInsertionSort(generateTestDataHalves(size), generateTestDataHalves(size*2));
+        double bValueInsertionHalfRandom = benchmarkInsertionSort(generateTestDataHalfRandom(size), generateTestDataHalfRandom(size*2));
+
+        double bValueShellSortBinary = benchmarkShellsort(generateTestDataBinary(size), generateTestDataBinary(size*2));
+        double bValueShellSortHalves = benchmarkShellsort(generateTestDataHalves(size), generateTestDataHalves(size*2));
+        double bValueShellSortHalfRandom = benchmarkShellsort(generateTestDataHalfRandom(size), generateTestDataHalfRandom(size*2));
+
+        System.out.println("         Insertion    Shellsort");
+        System.out.printf("Bin      %.4f       %.4f%n", bValueInsertionBinary, bValueShellSortBinary);
+        System.out.printf("Half     %.4f       %.4f%n", bValueInsertionHalves, bValueShellSortHalves);
+        System.out.printf("RanInt   %.4f       %.4f%n", bValueInsertionHalfRandom, bValueShellSortHalfRandom);
+    }
+
+
     /***************************************************************************
      * END - SORTING UTILITIES, DO NOT MODIFY                                  *
      **************************************************************************/
 
     //TODO: implement interface methods.
 
+
+
     public static void main(String args[]) {
         BenchmarkTool me = new CompletedBenchmarkTool();
-        int size = 4096;
+        int size = 8;
         
         //NOTE: feel free to change size here. all other code must go in the
         //      methods.
